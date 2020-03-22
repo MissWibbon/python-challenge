cb30346eac1b213ffa435639b3d65103726ec31e
Michelle <wuebben.michelle@yahoo.com>
2020-03-21 22:16:52 -0500

    added sum

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,13 +10,15 @@ Daily_Change = []
 Previous_Profit_Loss = 0
 with open(path) as data_file:
     data_csv = csv.reader(data_file)
-    header = next(data_csv)
 
+    header = next(data_csv)
+    
     for row in data_csv:
         total_month +=1
-        total_profit = int (row[1])
+        profit = int(row[1])
+        total_profit = total_profit + profit
         Daily_Change.append(int(row[1]) - Previous_Profit_Loss)
         Previous_Profit_loss = int(row[1])
 print("Total Month", total_month)
-print(f"Total: ${total_loss}")
+print(f"Total Profit: ", total_profit)
 print(f"Average Change: ", round(float(sum(Daily_Change)/len(Daily_Change)))*100)
